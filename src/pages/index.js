5767097aa450a47c9da209a6b9b193433f13a815
Maximilian <max-dz@hotmail.com>
2020-01-02 16:11:30 +0100

    Update 404 and index from class to function components

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,27 +4,25 @@ import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-class Home extends React.Component {
-  render() {
-    const { data } = this.props
-    const siteTitle = data.site.siteMetadata.title
+const Home = props => {
+  const { data } = props
+  const siteTitle = data.site.siteMetadata.title
 
-    return (
-      <Layout location={this.props.location} title={siteTitle}>
-        <SEO title="All posts" />
-        <p>home</p>
-        <Link className="btn btn-primary" to="/about">
-          About
-        </Link>
-        <Link className="btn btn-primary" to="/awards">
-          Awards
-        </Link>
-        <Link className="btn btn-primary" to="/history">
-          history
-        </Link>
-      </Layout>
-    )
-  }
+  return (
+    <Layout location={props.location} title={siteTitle}>
+      <SEO title="All posts" />
+      <p>home</p>
+      <Link className="btn btn-primary" to="/about">
+        About
+      </Link>
+      <Link className="btn btn-primary" to="/awards">
+        Awards
+      </Link>
+      <Link className="btn btn-primary" to="/history">
+        history
+      </Link>
+    </Layout>
+  )
 }
 
 export default Home
