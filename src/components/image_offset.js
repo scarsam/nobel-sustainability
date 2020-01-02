9fec559ebed41279cf8692bf212db83169399452
Sam Ojling <sam.ojling@zenpayroll.com>
2020-01-02 18:10:30 +0100

    Made background-color prop required

--- src/components/image_offset.js
+++ src/components/image_offset.js
@@ -5,7 +5,7 @@ import PropTypes from "prop-types"
 const ImageOffset = ({ src, backgroundColor, text }) => {
   return (
     <div className="relative offset-image-wrapper">
-      <Img className={"z-index-2"} fluid={src} />
+      <Img className="z-index-2" fluid={src} />
       {text && <p className="offset-image-text absolute z-index-3">{text}</p>}
       <div className={`absolute z-index-1 offset-bg ${backgroundColor}`} />
     </div>
@@ -14,7 +14,7 @@ const ImageOffset = ({ src, backgroundColor, text }) => {
 
 ImageOffset.propTypes = {
   src: PropTypes.object.isRequired,
-  backgroundColor: PropTypes.string,
+  backgroundColor: PropTypes.string.isRequired,
   text: PropTypes.string,
 }
 
