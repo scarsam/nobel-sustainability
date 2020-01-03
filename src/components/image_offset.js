586132602a8c776f56999962571820cad18c93e1
Sam Ojling <sam.ojling@zenpayroll.com>
2020-01-03 09:57:00 +0100

    Replaced scss color with color helper

--- src/components/image_offset.js
+++ src/components/image_offset.js
@@ -6,7 +6,9 @@ const ImageOffset = ({ src, backgroundColor, text }) => {
   return (
     <div className="relative offset-image-wrapper">
       <Img className="z-index-2" fluid={src} />
-      {text && <p className="offset-image-text absolute z-index-3">{text}</p>}
+      {text && (
+        <p className="offset-image-text absolute z-index-3 c-white">{text}</p>
+      )}
       <div className={`absolute z-index-1 offset-bg ${backgroundColor}`} />
     </div>
   )
