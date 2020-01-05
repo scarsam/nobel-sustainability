84c70f0859a7196b8fbbf5e26407100df2723665
Maximilian <max-dz@hotmail.com>
2020-01-05 23:04:25 +0100

    Add social link icons to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,11 +1,36 @@
 import React from "react"
+import logo from "../images/nobel-logo-white.svg"
+import { Link } from "gatsby"
+import facebook from "../images/social/facebook.svg"
+import instagram from "../images/social/instagram.svg"
+import linkedin from "../images/social/linkedin.svg"
+import twitter from "../images/social/twitter.svg"
+
+// TODO ADD CORRECT LINKS TO NST SOCIAL MEDIA
 
 const footer = () => {
   return (
-    <footer>
-      © {new Date().getFullYear()}, Built with
-      {` `}
-      <a href="https://www.gatsbyjs.org">Gatsby</a>
+    <footer className="c-white padding-top-20px">
+      <div className="container d-flex justify-content-between">
+        <div className="footer-title">
+          <img src={logo} alt="company logo white" />
+          <span className="padding-left-10px">Nobel Sustainability Trust</span>
+        </div>
+        <div className="social-links d-flex c-white">
+          <Link className="padding-right-15px">
+            <img src={facebook} alt="facebook icon" />
+          </Link>
+          <Link className="padding-right-15px">
+            <img src={instagram} alt=" instagram icon" />
+          </Link>
+          <Link className="padding-right-15px"> 
+            <img src={linkedin} alt="linkedin icon" />
+          </Link>
+          <Link className="padding-right-15px">
+            <img src={twitter} alt="twitter icon" />
+          </Link>
+        </div>
+      </div>
     </footer>
   )
 }
