fbc0ef542f887e1fc4472477db5c6f0a93ce96d8
Maximilian <max-dz@hotmail.com>
2020-01-05 17:37:12 +0100

    Remove container div from all pages
    Moved container div to Layout since it's used on all pages

--- src/pages/awards.js
+++ src/pages/awards.js
@@ -4,10 +4,8 @@ import Layout from "../components/layout"
 const Awards = () => {
   return (
     <Layout>
-      <div className="container">
-        <div className="col-4">
-          <h1>Awards</h1>
-        </div>
+      <div className="col-4">
+        <h1>Awards</h1>
       </div>
     </Layout>
   )
