f2116b6d05761f66112a5ae456e8ac719991e88a
Maximilian <max-dz@hotmail.com>
2020-01-06 16:58:22 +0100

    Add styling to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,14 @@ import SEO from "../components/seo"
 import Layout from "../components/layout"
 import ImageOffset from "../components/image_offset"
 import IntroText from "../components/intro_text"
+import eartCapitalLogo from "../images/partners/earth-capital-logo.svg"
+import lombardLogo from "../images/partners/lombard-logo.svg"
 
 const Home = props => {
   const { data } = props
   // const siteTitle = data.site.siteMetadata.title
-  const image = data.file.childImageSharp.fluid
+  const treesImage = data.trees.childImageSharp.fluid
+  const solarImage = data.solarImage.childImageSharp.fluid
 
   return (
     <Layout>
@@ -17,18 +20,125 @@ const Home = props => {
         headline="A world in balance"
         subheading="We bring public awareness to the issues of pollution and clean energy and encouraging change"
       />
+      <section id="banner">
+        <ImageOffset src={solarImage} backgroundColor={"bg-sand"} />
+      </section>
 
-      <div className="container">
-        <div className="col-4">
-          <ImageOffset
-            src={image}
-            backgroundColor={"bg-green"}
-            text={
-              "Creation a pollution-free, life-sustaining environment for all"
-            }
-          />
+      <section id="awards" className="container">
+        <div className="row">
+          <div className="col-md-2">
+            <h3>Sustainability Award</h3>
+          </div>
+          <div className="col-md-5">
+            <p>
+              Lorem ipsum dolor amet williamsburg adaptogen brooklyn small batch
+              etsy mlkshk sartorial biodiesel enamel pin chillwave blog viral
+              90's. Lomo ethical tousled skateboard swag master cleanse selvage
+              neutra scenester. Cronut plaid YOLO woke tbh wolf aesthetic.
+              Post-ironic banh mi cold-pressed hashtag roof party. Iceland vice
+              aesthetic, raclette meggings pork belly bicycle rights umami
+              tousled pickled hexagon blue bottle. Skateboard etsy succulents,
+              mumblecore flexitarian hammock hella pabst fashion axe tumeric
+              lo-fi swag cronut. Everyday carry blog YOLO air plant vegan
+              keffiyeh viral four dollar toast. Taiyaki vegan deep v, jean
+              shorts cronut vexillologist ennui skateboard ramps man bun selfies
+              church-key brooklyn.
+            </p>
+          </div>
+          <div className="col-md-5">
+            <p>
+              Lorem ipsum dolor amet williamsburg adaptogen brooklyn small batch
+              etsy mlkshk sartorial biodiesel enamel pin chillwave blog viral
+              90's. Lomo ethical tousled skateboard swag master cleanse selvage
+              neutra scenester. Cronut plaid YOLO woke tbh wolf aesthetic.
+              Post-ironic banh mi cold-pressed hashtag roof party. Iceland vice
+              aesthetic, raclette meggings pork belly bicycle rights umami
+              tousled pickled hexagon blue bottle. Skateboard etsy succulents,
+              mumblecore flexitarian hammock hella pabst fashion axe tumeric
+              lo-fi swag cronut.
+            </p>
+          </div>
         </div>
+      </section>
+
+      <section id="partners" className="bg-grey-beige">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-2 d-flex align-items-center">
+              <h3>Partners</h3>
+            </div>
+            <div className="col-md-3 col-6 d-flex align-items-center">
+              <img src={eartCapitalLogo} alt="Earth Captial company logo" />
+            </div>
+            <div className="col-md-3 col-6 d-flex align-items-center">
+              <img src={lombardLogo} alt="Lombard company logo" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="conference" className="container">
+        <div className="row">
+          <div className="col-md-2">
+            <h3>Sustainable Technology Conference</h3>
+          </div>
+          <div className="col-md-5">
+            <p>
+              Lorem ipsum dolor amet williamsburg adaptogen brooklyn small batch
+              etsy mlkshk sartorial biodiesel enamel pin chillwave blog viral
+              90's. Lomo ethical tousled skateboard swag master cleanse selvage
+              neutra scenester. Cronut plaid YOLO woke tbh wolf aesthetic.
+              Post-ironic banh mi cold-pressed hashtag roof party. Iceland vice
+              aesthetic, raclette meggings pork belly bicycle rights umami
+              tousled pickled hexagon blue bottle. Skateboard etsy succulents,
+              mumblecore flexitarian hammock hella pabst fashion axe tumeric
+              lo-fi swag cronut. Everyday carry blog YOLO air plant vegan
+              keffiyeh viral four dollar toast. Taiyaki vegan deep v, jean
+              shorts cronut vexillologist ennui skateboard ramps man bun selfies
+              church-key brooklyn.
+            </p>
+          </div>
+          <div className="col-md-5 col-lg-3 offset-lg-1">
+            <ImageOffset
+              src={treesImage}
+              backgroundColor={"bg-green"}
+              text={
+                "Creation a pollution-free, life-sustaining environment for all"
+              }
+            />
+          </div>
+        </div>
+      </section>
+
+      <div className="container padding-top-none padding-bottom-none">
+        <div className="bg-green section-divider"></div>
       </div>
+
+      <section id="news" className="container">
+        <div className="row">
+          <div className="col-md-2">
+            <h3>News</h3>
+          </div>
+          <div className="col-md-10">
+            <p className="c-green">March 24, 2018</p>
+            <p>
+              LONDON (Reuters) - Members of Sweden's Nobel family, best known
+              for the prizes bearing their name, and the billionaire co-founter
+              of...
+            </p>
+            <p>Read More</p>
+          </div>
+          <div className="offset-md-2 col-md-10">
+            <p className="c-green">March 24, 2018</p>
+            <p>
+              LONDON (Reuters) - Members of Sweden's Nobel family, best known
+              for the prizes bearing their name, and the billionaire co-founter
+              of...
+            </p>
+            <p>Read More</p>
+          </div>
+        </div>
+      </section>
     </Layout>
   )
 }
@@ -42,16 +152,16 @@ export const pageQuery = graphql`
         title
       }
     }
-    file(relativePath: { eq: "trees.jpg" }) {
+    trees: file(relativePath: { eq: "trees.jpg" }) {
       childImageSharp {
-        fluid(maxWidth: 200) {
+        fluid(maxWidth: 960) {
           ...GatsbyImageSharpFluid
         }
       }
     }
-    file(relativePath: { eq: "trees.jpg" }) {
+    solarImage: file(relativePath: { eq: "building.jpg" }) {
       childImageSharp {
-        fluid(maxWidth: 200) {
+        fluid(maxWidth: 960, maxHeight: 140) {
           ...GatsbyImageSharpFluid
         }
       }
