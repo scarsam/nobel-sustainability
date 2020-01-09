cde825bb38a461b43c2156707cc2d33cf90f1561
Maximilian <max-dz@hotmail.com>
2020-01-09 20:56:06 +0100

    Add scrollTop component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,17 @@
 import React from "react"
 import Header from "../components/header"
 import Footer from "../components/footer"
+import ScrollTop from "../components/scroll_top"
 import "../styles/nobel-sustainability.scss"
 
 const Layout = ({ children }) => {
   return (
-    <React.Fragment>
+    <>
       <Header />
       <main>{children}</main>
+      <ScrollTop />
       <Footer />
-    </React.Fragment>
+    </>
   )
 }
 
