eadc5e79acb1243fe91ce00509db478612689b7e
Sam Ojling <sam.ojling@zenpayroll.com>
2020-01-16 11:17:18 +0100

    Updating folder path for assets

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,7 +35,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        path: `${__dirname}/content/images`,
+        path: `${__dirname}/content/assets`,
         name: `img`,
       },
     },
