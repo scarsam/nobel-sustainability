718c36d9ef46501f982de05cb3a30c48391c6928
Maximilian <max-dz@hotmail.com>
2020-01-19 17:14:34 +0100

    Test rendering markdown on contactpage. Works, but not as expected

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { graphql } from 'gatsby'
 
 const Contact = ({ data }) => {
+  const { html } = data.allMarkdownRemark.edges[0].node
+  console.log(html)
   return (
     <Layout>
-      <div className="col-4">
-        <h1>Contact</h1>
+      <div className="container">
+        <div
+          className="col-md-6"
+          dangerouslySetInnerHTML={{ __html: html }}
+        ></div>
       </div>
     </Layout>
   )
@@ -23,7 +29,7 @@ export const pageQuery = graphql`
     allMarkdownRemark(
       limit: 1
       sort: { order: DESC, fields: [frontmatter___date] }
-      filter: { fileAbsolutePath: { regex: "/history/" } }
+      filter: { fileAbsolutePath: { regex: "/contact/" } }
     ) {
       edges {
         node {
