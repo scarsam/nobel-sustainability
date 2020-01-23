2a37f59adde8cb20af9cd8e8161665e5685e44e2
Maximilian <max-dz@hotmail.com>
2020-01-23 23:30:38 +0100

    Add component to render images on preview-pages.
    ImageInfo is a promise and I am not sure where and how to handle this

---
+++ src/cms/preview-components/PreviewCompatibleImage.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Img from 'gatsby-image'
+
+const PreviewCompatibleImage = ({ imageInfo, type }) => {
+  const { alt = '', childImageSharp, image } = imageInfo
+
+  if (type === 'fixed') {
+    if (!!image && !!image.childImageSharp) {
+      return <Img fixed={image.childImageSharp.fixed} alt={alt} />
+    }
+
+    if (!!childImageSharp) {
+      return <Img fixed={childImageSharp.fixed} alt={alt} />
+    }
+  }
+
+  if (type === 'fluid') {
+    if (!!image && !!image.childImageSharp) {
+      return <Img fluid={image.childImageSharp.fluid} alt={alt} />
+    }
+
+    if (!!childImageSharp) {
+      return <Img fluid={childImageSharp.fluid} alt={alt} />
+    }
+  }
+
+  if (!!image && typeof image === 'string') return <img src={image} alt={alt} />
+
+  return null
+}
+
+export default PreviewCompatibleImage
