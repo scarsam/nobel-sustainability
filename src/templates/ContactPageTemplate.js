67e6fb5cc0aa8ce189ec4ba21862cddd4563dfc4
Sam Ojling <sam.ojling@zenpayroll.com>
2020-01-26 12:37:34 -0800

    Made a basic contact page

--- src/templates/ContactPageTemplate.js
+++ src/templates/ContactPageTemplate.js
@@ -5,29 +5,60 @@ const ContactPageTemplate = ({ title, officeOne, officeTwo, emails }) => {
   console.log(officeOne)
   return (
     <Layout>
-      <div className="container">
-        <h1>{title}</h1>
-        <div>
-          <p>{officeOne.name}</p>
-          <p>{officeOne.street}</p>
-          <p>
-            {officeOne.zip} {officeOne.city}, {officeOne.country}
-          </p>
+      <h1 className="text-center padding-top-50px padding-bottom-50px">
+        {title}
+      </h1>
+      <section className="bg-sand">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-3 col-lg-2 offset-lg-2">
+              <h3 className="padding-bottom-20px padding-bottom-md-none">
+                Offices
+              </h3>
+            </div>
+            <div className="col-md-4">
+              <h4>{officeOne.name}</h4>
+              <p>
+                {officeOne.street}
+                <span className="d-block">
+                  {officeOne.zip} {officeOne.city}, {officeOne.country}
+                </span>
+              </p>
+            </div>
+            <div className="col-md-4">
+              <h4>{officeTwo.name}</h4>
+              <p>
+                {officeTwo.street}
+                <span className="d-block">
+                  {officeTwo.zip} {officeTwo.city}, {officeTwo.country}
+                </span>
+              </p>
+            </div>
+          </div>
         </div>
-        <div>
-          <p>{officeTwo.name}</p>
-          <p>{officeTwo.street}</p>
-          <p>
-            {officeTwo.zip} {officeTwo.city}, {officeTwo.country}
-          </p>
+      </section>
+      <section>
+        <div className="container">
+          <div className="row padding-top-40px">
+            <div className="col-md-3 col-lg-2 offset-lg-2">
+              <h3 className="padding-bottom-20px padding-bottom-md-none">
+                Email
+              </h3>
+            </div>
+            {emails &&
+              emails.map((user, index) => {
+                return (
+                  <div key={index} className="col-md-4">
+                    <h4>{user.name}</h4>
+                    <p>
+                      <a href={`mailto:${user.email}`}>{user.email}</a>
+                    </p>
+                  </div>
+                )
+              })}
+          </div>
         </div>
-        {emails.map(email => (
-          <>
-            <p>{email.email}</p>
-            <p>{email.name}</p>
-          </>
-        ))}
-      </div>
+      </section>
     </Layout>
   )
 }
