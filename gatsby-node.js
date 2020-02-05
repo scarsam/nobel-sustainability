4c530fe382cf38c7290d272aa29703ce182b9d5e
Maximilian <max-dz@hotmail.com>
2020-02-05 22:31:01 +0100

    Rename to to ArticleTemplate

--- gatsby-node.js
+++ gatsby-node.js
@@ -27,10 +27,9 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `)
   result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-    console.log(node)
     createPage({
       path: node.fields.slug,
-      component: path.resolve(`./src/templates/NewsPageTemplate.js`),
+      component: path.resolve(`./src/templates/ArticleTemplate.js`),
       context: {
         // Data passed to context is available
         // in page queries as GraphQL variables.
---
+++ src/templates/ArticleTemplate.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
+import ReactMarkdown from 'react-markdown'
+
+const ArticleTemplate = ({ data }) => {
+  const post = data.markdownRemark.frontmatter
+
+  return (
+    <Layout>
+      <div className="container">
+        <div className="col-md-10">
+          <h3>{post.title}</h3>
+          <p className="c-green">{post.date}</p>
+          <div>
+            <ReactMarkdown source={post.text} />
+          </div>
+        </div>
+      </div>
+    </Layout>
+  )
+}
+
+export default ArticleTemplate
+
+export const PageQuery = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      frontmatter {
+        title
+        date
+        text
+      }
+      fields {
+        slug
+      }
+    }
+  }
+`
