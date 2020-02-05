c1e47f3e5514982d80f32f9ed49e8cebad769000
Maximilian <max-dz@hotmail.com>
2020-02-05 22:31:47 +0100

    Add NewsPageTemplate

--- src/pages/news.js
+++ src/pages/news.js
@@ -1,5 +1,6 @@
-import React from "react"
-import Layout from "../components/layout"
+import React from 'react'
+import Layout from '../components/layout'
+import NewsPageTemplate from '../templates/NewsPageTemplate'
 
 const News = () => {
   return (
