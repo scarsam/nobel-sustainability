bcae9905ee1d4b250d2562b1ec90e9697f5208a8
Maximilian <max-dz@hotmail.com>
2020-03-05 15:19:32 +0100

    Remove scroll-top component from layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,15 +1,13 @@
-import React from "react"
-import Header from "../components/header"
-import Footer from "../components/footer"
-import ScrollTop from "../components/scroll_top"
-import "../styles/nobel-sustainability.scss"
+import React from 'react'
+import Header from '../components/header'
+import Footer from '../components/footer'
+import '../styles/nobel-sustainability.scss'
 
 const Layout = ({ children }) => {
   return (
     <>
       <Header />
       <main>{children}</main>
-      <ScrollTop />
       <Footer />
     </>
   )
